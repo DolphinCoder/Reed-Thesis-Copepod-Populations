64fd312c929d04e506e9280cda19664fe089b713
Ella <eleanor.crotty@gmail.com>
2025-02-21 23:27:45 -0800

    LiveOcean + Collapse Day
    Worked on LiveOcean model, trying to extract depth. Collapsed samples taken at the same datetime.

--- NetCDF_to_DF.R
+++ NetCDF_to_DF.R
@@ -45,21 +45,33 @@ head(s_rho) # no idea what this means
 
 tunits <- ncatt_get(ncin,"ocean_time","units")
 
+# DO in mmol/m^3 is oxygen
 ounits <- ncatt_get(ncin, oxygen,"units")
 ox_array <- ncvar_get(ncin, oxygen)
 ofillvalue <- ncatt_get(ncin, oxygen, "_FillValue")
+ox_array[ox_array==ofillvalue$value] <- NA
 
+# temp in C is temperature
 tunits <- ncatt_get(ncin, temp, "units")
 temp_array <- ncvar_get(ncin, temp)
 tfillvalue <- ncatt_get(ncin, temp, "_FillValue")
+temp_array[temp_array==tfillvalue$value] <- NA
+
+# z_rho has depth information
+# https://www.myroms.org/wiki/Numerical_Solution_Technique
+dunits <- ncatt_get(ncin, temp, "units")
+dep_array <- ncvar_get(ncin, temp)
+dfillvalue <- ncatt_get(ncin, temp, "_FillValue")
+dep_array[dep_array==dfillvalue$value] <- NA
+
+# z_w has depth information
+# Not dealing with it currently bc it needs s_w coordinates
+# instead of [s_rho, ocean_time] and the current script isn't built for that
+
 
 nc_close(ncin)
 
 time <- as.POSIXct(ocean_time, tz = "UTC", origin = "1970-01-01") # those are the times we wanted! huzzah.
-
-temp_array[temp_array==tfillvalue$value] <- NA
-
-ox_array[ox_array==ofillvalue$value] <- NA
 
 # each variable has coordinates [s_rho, ocean_time] that it can be grabbed by
 temp_slice <- temp_array[,1] # first column
@@ -96,9 +108,13 @@ df4 <- df3 %>%
          Date_UTC = as.POSIXct(Date_UTC, tz = "UTC"), 
          Temp_C = as.numeric(Temp_C))
 
+# Now add depth
+Depth <- as.vector(dep_array)
+df5 <- data.frame(cbind(df4, Depth))
+
 # Now convert oxygen
 # See lab notebook, but mmol/m^3 x 0.015999 = mg/L oxygen
-final_df <- df3 %>% 
+final_df <- df5 %>% 
   mutate(Oxygen_mg_L = Oxygen_mmol_m3 * 0.015999) # %>% # these seem legit
   # filter(s_rho > -0.92 & s_rho < -0.97) # saving for later, might as well export
 
@@ -107,4 +123,4 @@ final_df <- df3 %>%
 # i do Not wanna do all that math. mooring is close to the bottom so let's use -0.95 rho
 
 
-write_csv(final_df, here("LiveOceanTH042_2021_2023.csv"))
+write.csv(final_df, here("LiveOceanTH042_2021_2023.csv"))
